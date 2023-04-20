2572734165d90eb0b3c8f733ec54561ced56548c
Dariomnz <dario.mnz.mnz@gmail.com>
2023-04-20 18:36:44 +0200

    Try add MockHMD and UI

---
+++ Assets/Scripts/TestButtom.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtom : MonoBehaviour
+{
+    public void DebugPrint()
+    {
+        Debug.Log("Click");
+    }
+}
