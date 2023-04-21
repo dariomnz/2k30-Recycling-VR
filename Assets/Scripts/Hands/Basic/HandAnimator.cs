413f41380c35a4f28f94c09e95919b4020b1e4b0
Dariomnz <dario.mnz.mnz@gmail.com>
2023-04-21 02:37:01 +0200

    Added hands

---
+++ Assets/Scripts/Hands/Basic/HandAnimator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandAnimator : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public ActionBasedController Controller = null;
+
+    private Animator animator = null;
+
+    private readonly List<Finger> gripFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Middle),
+        new Finger(FingerType.Ring),
+        new Finger(FingerType.Pinky)
+    };
+
+    private readonly List<Finger> pointFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Index),
+        new Finger(FingerType.Thumb)
+    };
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Store input
+        CheckGrip();
+        CheckPointer();
+
+        // Smooth input 
+        SmoothFinger(gripFingers);
+        SmoothFinger(pointFingers);
+
+        // Apply smooth values
+        AnimateFinger(gripFingers);
+        AnimateFinger(pointFingers);
+
+    }
+
+    private void CheckGrip()
+    {
+        float gripValue = Controller.selectActionValue.action.ReadValue<float>();
+        SetFingerTargets(gripFingers, gripValue);
+    }
+
+    private void CheckPointer()
+    {
+        float pointerValue = Controller.activateActionValue.action.ReadValue<float>();
+        // if (Controller.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float pointerValue))
+        SetFingerTargets(pointFingers, pointerValue);
+    }
+
+    private void SetFingerTargets(List<Finger> fingers, float value)
+    {
+        foreach (Finger finger in fingers)
+        {
+            finger.target = value;
+        }
+    }
+
+    private void SmoothFinger(List<Finger> fingers)
+    {
+        foreach (Finger finger in fingers)
+        {
+            float time = speed * Time.unscaledDeltaTime;
+            finger.current = Mathf.MoveTowards(finger.current, finger.target, time);
+        }
+    }
+
+    private void AnimateFinger(List<Finger> fingers)
+    {
+        foreach (Finger finger in fingers)
+            AnimateFinger(finger.type.ToString(), finger.current);
+    }
+
+    private void AnimateFinger(string finger, float blend)
+    {
+        animator.SetFloat(finger, blend);
+    }
+}
---
+++ Assets/Scripts/Hands/Basic/HandHider.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandHider : MonoBehaviour
+{
+    public GameObject handObject = null;
+
+    private HandPhysics handPhysics = null;
+    private XRDirectInteractor interactor = null;
+
+    private void Awake()
+    {
+        handPhysics = handObject.GetComponent<HandPhysics>();
+        interactor = GetComponent<XRDirectInteractor>();
+    }
+
+    private void OnEnable()
+    {
+        interactor.selectEntered.AddListener(Hide);
+        interactor.selectExited.AddListener(Show);
+    }
+
+    private void OnDisable()
+    {
+        interactor.selectEntered.RemoveListener(Hide);
+        interactor.selectExited.RemoveListener(Show);
+    }
+
+    private void Show(SelectExitEventArgs interactable)
+    {
+        handPhysics.TeleportToTarget();
+        Invoke(nameof(DelayShow), 0.2f);
+    }
+
+    private void Hide(SelectEnterEventArgs interactable)
+    {
+        handObject.GetComponent<Collider>().enabled = false;
+    }
+
+    private void DelayShow()
+    {
+        handObject.GetComponent<Collider>().enabled = true;
+    }
+}
